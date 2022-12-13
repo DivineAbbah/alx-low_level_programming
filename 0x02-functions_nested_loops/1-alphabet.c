df0d1ce833029cbdba91a222df67b55dfd99c7de
Abbah Divine <abbahattamah>
2022-12-13 12:01:47 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Function that prints alphabets
+ *
+ * Description: to print alphabets
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
