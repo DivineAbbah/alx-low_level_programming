469ed1dfcb2f83eb6ff85a284dea6a0c4e8c4c3d
Abbah Divine <abbahattamah@gmail.com>
2022-12-15 16:36:34 -0800

    5th release

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints 0 - 9 except 2, 4
+* Description: prints
+* Return: void
+*/
+
+void print_most_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		if (c != '2' && c != '4')
+		{
+			_putchar(c);
+		}
+	c++;
+	}
+_putchar('\n');
+}
