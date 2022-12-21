095f23b8adec5cb1c92e99ab00a6bfcbf6ebc9ab
Abbah Divine <abbahattamah@gmail.com>
2022-12-21 00:24:46 -0800

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+/**
+ * _strncat - function to concatenate based on the conditional value
+ *
+ * @dest: destination string
+ * @src: source string variable
+ * @n: conditional variable
+ * Return: the string to be returned
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y;
+
+	y = 0;
+	for (x = 0; dest[x] != '\0'; x++)
+		;
+
+	while (src[y] != '\0' && y < n)
+	{
+		dest[x + y] = src[y];
+		y++;
+	}
+	return (dest);
+}
