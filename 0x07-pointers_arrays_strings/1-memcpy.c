8c39ae09a66ed79683ebe49b8d4d48b25dc83714
Abbah Divine <Abbahattamah@gmail.com>
2023-01-02 05:56:42 -0800

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy -copies memory area
+ *
+ * @dest: the mem arera to copy to
+ * @n: the no of bytes to copy
+ * @src: the mem area to copy from
+ *
+ * Return: a pointer to the dest variable
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+		dest[x] = src[x]
+
+	return (dest);
+}
