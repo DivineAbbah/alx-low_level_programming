3626e68baffad3eb7e32d0782de730b2e8583794
Abbah Divine <Abbahattamah@gmail.com>
2023-01-13 11:22:36 -0800

    2-calloc.c commit'

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: The number of elements.
+ * @size: The byte size of each array element.
+ *
+ * Return: Null If nmemb = 0, size = 0 else a memory pointer.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *alloc;
+	char *ptrr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	alloc = malloc(size * nmemb);
+
+	if (alloc == NULL)
+		return (NULL);
+
+	ptrr = alloc;
+
+	for (i = 0; i < (size * nmemb); i++)
+		ptrr[i] = '\0';
+
+	return (alloc);
+}
