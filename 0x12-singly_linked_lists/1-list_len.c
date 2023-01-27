b1975472b8a3d4802c01d8cdd0c20cd8bcd4dcfb
Abbah Divine <abbahattamah@gmail.com>
2023-01-27 07:15:47 -0800

    1-list_len.c commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h
+
+/**
+ * list_len - get number of nodes in the list
+ * @h: singly linked list
+ * Return: number of elements in a linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
